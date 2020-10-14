48b06af037cfa309ff206a96861442ce55894469
Nicholas Zhaosi <weihl18@lzu.edu.cn>
2020-10-14 21:07:46 +0800

    验证了监听状态以及展示在状态栏中，（调试成功）

--- src/extension.ts
+++ src/extension.ts
@@ -5,10 +5,44 @@ import * as vscode from 'vscode';
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
+	var isBlockState = 1;
+
+	const myStatusBarItem=vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Right, 100);
+
+	const DecideState = () => {
+		if (isBlockState % 2 == 0) {
+			myStatusBarItem.text="处于激活状态";
+			myStatusBarItem.show();
+			return true; //偶数
+		}
+		else {
+			myStatusBarItem.text="处于非激活状态";
+			myStatusBarItem.show();
+			return false; //奇数
+		}
+	}
+
+	const ChangeState = () => {
+		isBlockState++;
+		DecideState()
+	}
 
 
-	
+	const test=()=>{
+		myStatusBarItem.text="激活状态";
+		myStatusBarItem.show();
+		vscode.window.showInformationMessage("已经执行")
+	}
+
+	//  以下是注册命令的部分
+	const registerTest=vscode.commands.registerCommand("code-block.Test",test)
+	context.subscriptions.push(registerTest)
+
+	const registerChangeState=vscode.commands.registerCommand("code-block.ChangeState",ChangeState);
+	context.subscriptions.push(registerChangeState)
+
+
 }
 
 // this method is called when your extension is deactivated
-export function deactivate() {}
+export function deactivate() { }
