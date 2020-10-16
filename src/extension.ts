dc9f6b97ef95a60e06ab17923e82e4de44ad10d5
Nicholas Zhaosi <weihl18@lzu.edu.cn>
2020-10-16 18:42:38 +0800

    第一次成功发布

--- src/extension.ts
+++ src/extension.ts
@@ -1,11 +1,12 @@
+/* eslint-disable eqeqeq */
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
-import { ProcessLineNumberArray, Interval } from './mathematics'
+import { ProcessLineNumberArray, Interval } from './mathematics';
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
-export function activate(context: vscode.ExtensionContext) {
+export function activate(context: vscode.ExtensionContext){
 	var isBlockState = 1;
 
 	const myStatusBarItem = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Right, 100);
@@ -21,21 +22,21 @@ export function activate(context: vscode.ExtensionContext) {
 			myStatusBarItem.show();
 			return false; //奇数
 		}
-	}
+	};
 
 	const ChangeState = () => {
 		isBlockState++;
-		DecideState()
-	}
+		DecideState();
+	};
 
 
 	// 分割整个activeEditor的内容（按行），然后逐行执行匹配，从而获得行号
 	const editor = vscode.window.activeTextEditor;
 
-	var reg = /^\s*%+\*+%+\s*$/
+	var reg = /^\s*%+\*+%+\s*$/;
 
 
-	const getLineNumbers = () => {
+	const getLineNumbers = ()=> {
 
 		if (editor) {
 			var text = editor.document.getText();
@@ -48,9 +49,9 @@ export function activate(context: vscode.ExtensionContext) {
 					count.push(index);
 				}
 			}
+			return {count,Length};
 		}
-		return { count, Length };
-	}
+	};
 
 	//接下来处理头尾行，只是编写一个函数
 
@@ -62,75 +63,75 @@ export function activate(context: vscode.ExtensionContext) {
 	//并且我这里还没有考虑空文本
 
 	//初始化一组变量
-	var getLineObject = getLineNumbers()
-	var array = getLineObject.count
+	var getLineObject:any = getLineNumbers();
+	var array = getLineObject.count;
 	ProcessLineNumberArray(array, getLineObject.Length);
 
 	//定义一个listener函数来监听文本的改变
 	const updateLineNumberArray = () => {
 		getLineObject = getLineNumbers();
-		array = getLineObject.count
+		array = getLineObject.count;
 		ProcessLineNumberArray(array, getLineObject.Length);
-	}
-	vscode.window.onDidChangeTextEditorSelection(updateLineNumberArray)
+	};
+	vscode.window.onDidChangeTextEditorSelection(updateLineNumberArray);
 
 	//接下来实现高亮当前单元格
 	const decorationType = vscode.window.createTextEditorDecorationType({
 		backgroundColor: "#ac72dba8",
 		isWholeLine: true
-	})
+	});
 
 	const doNothingDecorationType = vscode.window.createTextEditorDecorationType({
 		isWholeLine: true,
 		backgroundColor: ""
-	})
+	});
 
 	const highLightCurrentBlock = () => {
 
-		let editor = vscode.window.activeTextEditor
+		let editor = vscode.window.activeTextEditor;
 		if (editor) {
 			let cursor = editor.selection.active;
 			let interval = Interval(array, cursor.line);
 			let position1 = new vscode.Position(interval[0], 0);
 			let position2 = new vscode.Position(interval[1], 0);
 			let range = new vscode.Range(position1, position2);
-			let decorationsArray: vscode.DecorationOptions[] = []
-			let decoration = { range }
-			decorationsArray.push(decoration)
+			let decorationsArray: vscode.DecorationOptions[] = [];
+			let decoration = { range };
+			decorationsArray.push(decoration);
 			//editor.setDecorations(decorationType,decorationsArray)
 			if (DecideState() == true) {
 				if (interval[0] == interval[1]) {
 					//分类讨论 因为如果代码格只有1格的话，也会返回相等
 					if (array.indexOf(interval[0]) > -1) {
-						editor.setDecorations(decorationType, [])
-						editor.setDecorations(doNothingDecorationType, decorationsArray)
+						editor.setDecorations(decorationType, []);
+						editor.setDecorations(doNothingDecorationType, decorationsArray);
 					}
 					else {
-						editor.setDecorations(decorationType, decorationsArray)
-						editor.setDecorations(doNothingDecorationType, [])
+						editor.setDecorations(decorationType, decorationsArray);
+						editor.setDecorations(doNothingDecorationType, []);
 					}
 				}
 				else {
-					editor.setDecorations(decorationType, decorationsArray)
-					editor.setDecorations(doNothingDecorationType, [])
+					editor.setDecorations(decorationType, decorationsArray);
+					editor.setDecorations(doNothingDecorationType, []);
 				}
 			}
 			else{
-				editor.setDecorations(decorationType, [])
-				editor.setDecorations(doNothingDecorationType,[])
+				editor.setDecorations(decorationType, []);
+				editor.setDecorations(doNothingDecorationType,[]);
 			}
 		}
 
-	}
+	};
 	// 接下来监听选择改变
-	vscode.window.onDidChangeTextEditorSelection(highLightCurrentBlock)
+	vscode.window.onDidChangeTextEditorSelection(highLightCurrentBlock);
 
 	//以上代码都还没有考虑激活问题，后续加上去应该不难
 
 	//以下来实现快速在单元格之间导航的内容，分为两部分，经过查找，VScode好像没有直接监听按键行为的api
 	const navigateThroughBlocksDown = () => {
 		if (DecideState() == true) {
-			let editor = vscode.window.activeTextEditor
+			let editor = vscode.window.activeTextEditor;
 			if (editor) {
 				let cursor = editor.selection.active;
 				let interval = Interval(array, cursor.line);
@@ -144,9 +145,9 @@ export function activate(context: vscode.ExtensionContext) {
 						}
 						else {
 							//如果不是最后一行,那么往下一格首行移动
-							let newPosition = new vscode.Position(interval[1] + 2, 0)
-							let newSelection = new vscode.Selection(newPosition, newPosition)
-							editor.selection = newSelection
+							let newPosition = new vscode.Position(interval[1] + 2, 0);
+							let newSelection = new vscode.Selection(newPosition, newPosition);
+							editor.selection = newSelection;
 						}
 					}
 					else {
@@ -156,9 +157,9 @@ export function activate(context: vscode.ExtensionContext) {
 						}
 						else {
 							//如果还不是最后一格，那么就去到下边界的下一格
-							let newPosition = new vscode.Position(interval[1] + 2, 0)
-							let newSelection = new vscode.Selection(newPosition, newPosition)
-							editor.selection = newSelection
+							let newPosition = new vscode.Position(interval[1] + 2, 0);
+							let newSelection = new vscode.Selection(newPosition, newPosition);
+							editor.selection = newSelection;
 						}
 					}
 				}
@@ -169,9 +170,9 @@ export function activate(context: vscode.ExtensionContext) {
 					}
 					else {
 						//如果还不是最后一格，那么就去到下边界的下一格
-						let newPosition = new vscode.Position(interval[1] + 2, 0)
-						let newSelection = new vscode.Selection(newPosition, newPosition)
-						editor.selection = newSelection
+						let newPosition = new vscode.Position(interval[1] + 2, 0);
+						let newSelection = new vscode.Selection(newPosition, newPosition);
+						editor.selection = newSelection;
 					}
 				}
 			}
@@ -180,11 +181,11 @@ export function activate(context: vscode.ExtensionContext) {
 		else {
 			// do nothing
 		}
-	}
+	};
 
 	const navigateThroughBlocksUp = () => {
 		if (DecideState() == true) {
-			let editor = vscode.window.activeTextEditor
+			let editor = vscode.window.activeTextEditor;
 			if (editor) {
 				let cursor = editor.selection.active;
 				let interval = Interval(array, cursor.line);
@@ -195,39 +196,39 @@ export function activate(context: vscode.ExtensionContext) {
 					if (interval[0] == interval[1]) {
 						if (array.indexOf(interval[0]) > -1) {
 							//如果是分隔符,往上一格末行
-							let newPosition = new vscode.Position(interval[0] - 2, 0)
-							let newSelection = new vscode.Selection(newPosition, newPosition)
-							editor.selection = newSelection
+							let newPosition = new vscode.Position(interval[0] - 2, 0);
+							let newSelection = new vscode.Selection(newPosition, newPosition);
+							editor.selection = newSelection;
 						}
 						else {
 							//如果不是分隔符，那么当做单元格来处理,去到上一格的末行
-							let newPosition = new vscode.Position(interval[0] - 2, 0)
-							let newSelection = new vscode.Selection(newPosition, newPosition)
-							editor.selection = newSelection
+							let newPosition = new vscode.Position(interval[0] - 2, 0);
+							let newSelection = new vscode.Selection(newPosition, newPosition);
+							editor.selection = newSelection;
 						}
 					}
 					else {
 						//此时肯定在单元格之中
-						let newPosition = new vscode.Position(interval[0] - 2, 0)
-						let newSelection = new vscode.Selection(newPosition, newPosition)
-						editor.selection = newSelection
+						let newPosition = new vscode.Position(interval[0] - 2, 0);
+						let newSelection = new vscode.Selection(newPosition, newPosition);
+						editor.selection = newSelection;
 					}
 				}
 			}
 		}
-	}
+	};
 
 
 	//  以下是注册命令的部分
 
 	const registerChangeState = vscode.commands.registerCommand("code-block.ChangeState", ChangeState);
-	context.subscriptions.push(registerChangeState)
+	context.subscriptions.push(registerChangeState);
 
-	const registernavigateThroughBlocksDown = vscode.commands.registerCommand("code-block.navigateThroughBlocksDown", navigateThroughBlocksDown)
-	context.subscriptions.push(registernavigateThroughBlocksDown)
+	const registernavigateThroughBlocksDown = vscode.commands.registerCommand("code-block.navigateThroughBlocksDown", navigateThroughBlocksDown);
+	context.subscriptions.push(registernavigateThroughBlocksDown);
 
-	const registernavigateThroughBlocksUp = vscode.commands.registerCommand("code-block.navigateThroughBlocksUp", navigateThroughBlocksUp)
-	context.subscriptions.push(registernavigateThroughBlocksUp)
+	const registernavigateThroughBlocksUp = vscode.commands.registerCommand("code-block.navigateThroughBlocksUp", navigateThroughBlocksUp);
+	context.subscriptions.push(registernavigateThroughBlocksUp);
 
 }
 
