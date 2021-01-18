46d08202747b8ce853baa1f1ec3782669089e340
weihongliang233 <weihl18@lzu.edu.cn>
2021-01-18 23:21:59 +0800

    多种语言可以对应不同的正则表达分隔符

--- src/extension.ts
+++ src/extension.ts
@@ -1,27 +1,29 @@
 /* eslint-disable eqeqeq */
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
-import * as vscode from 'vscode';
-import { ProcessLineNumberArray, Interval } from './mathematics';
+import * as vscode from "vscode";
+import { ProcessLineNumberArray, Interval } from "./mathematics";
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
 	var isBlockState = 1;
 
-	const myStatusBarItem = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Right, 100);
+	const myStatusBarItem = vscode.window.createStatusBarItem(
+		vscode.StatusBarAlignment.Right,
+		100
+	);
 
 	//	通过奇偶数来判断用户是否需要高亮当前单元格。 设置ChangeState,将它和一个快捷键绑定起来，每当用户按下快捷键的时候，就改变一次状态，状态分为奇偶，分别代表不激活和激活。
 	const DecideState = () => {
 		if (isBlockState % 2 == 0) {
 			myStatusBarItem.text = "Block Enabled";
 			myStatusBarItem.show();
-			return true; 
-		}
-		else {
+			return true;
+		} else {
 			myStatusBarItem.text = "Block Unabled";
 			myStatusBarItem.show();
-			return false; 
+			return false;
 		}
 	};
 
@@ -30,24 +32,53 @@ export function activate(context: vscode.ExtensionContext) {
 		DecideState();
 	};
 
-	//	这里我试图逃避类型检查，因为我不知道如何强制告诉ts regstring不会为undefined.
-	var regstring: any = vscode.workspace.getConfiguration("code-block").get("regularexpression");
+	//以下来读取正则表达式
+	/*
+	要有如下几个功能点：
+	1.在settings.json文件里面可以配置多种语言的正则表达式。当编辑器语言发生改变时，正则表达式会自动改变。为此需要用到onDidOpenTextDocument这个事件，在语言发生改变它会被激活。
+	2.需要onDidChangeConfiguration来监听settings.json的改变。
+	*/
 	var reg: RegExp;
+	var defaultStrList = [
+		"matlab:^\\s*%+\\*+%+\\s*$",
+		"wolfram:\\(\\*\\s\\s\\*\\)",
+	];
+	var dic: any;
+
 	const changeReg = () => {
-		if (regstring) {
-			reg = new RegExp(regstring);
+		let regStrList: string[] | undefined = vscode.workspace
+		.getConfiguration("code-block")
+		.get("regularexpression");
+		if(!regStrList){
+			regStrList=defaultStrList;
+			vscode.window.showInformationMessage("You havn't specify the regular expression delimiters, the defalut rules will be used");
 		}
-		else {
-			reg = /^\s*%+\*+%+\s*$/;
+		dic = {};
+		for (let str of regStrList) {
+			let language = str.split(":", 1)[0];
+			let reg = str.substring(language.length+1, str.length +1);
+			dic[language] = reg;
 		}
-		vscode.window.showInformationMessage("The Regular Expression has changed");
+		let editor = vscode.window.activeTextEditor;
+		if (editor) {
+			reg = new RegExp(dic[editor.document.languageId]);
+			if (!reg){
+				reg=new RegExp("");
+				vscode.window.showInformationMessage("You havn't specify the regular expression delimiter for the language currently used. By default, \"\" will be used");
+			}
+		} else {
+			vscode.window.showInformationMessage("Please open a file before using the code-block extension");
+		}
 	};
+
 	//监听changeReg函数，使得每次修改settings.json它都会立刻生效。
 	changeReg();
+	vscode.window.onDidChangeActiveTextEditor(changeReg);
 	vscode.workspace.onDidChangeConfiguration(changeReg);
+	vscode.workspace.onDidOpenTextDocument(changeReg);
 
 	//这个函数用来得到分割符号的位置，提供给后续处理
-	const getLineNumbers = (editor: vscode.TextEditor) => {  
+	const getLineNumbers = (editor: vscode.TextEditor) => {
 		var text = editor.document.getText();
 		var reshapedText = text.split("\n");
 		var Length = reshapedText.length;
@@ -71,12 +102,12 @@ export function activate(context: vscode.ExtensionContext) {
 	}
 	var decorationType = vscode.window.createTextEditorDecorationType({
 		backgroundColor: color,
-		isWholeLine: true
+		isWholeLine: true,
 	});
 
 	var doNothingDecorationType = vscode.window.createTextEditorDecorationType({
 		isWholeLine: true,
-		backgroundColor: ""
+		backgroundColor: "",
 	});
 
 	const changeColor = () => {
@@ -86,11 +117,11 @@ export function activate(context: vscode.ExtensionContext) {
 		}
 		decorationType = vscode.window.createTextEditorDecorationType({
 			backgroundColor: color,
-			isWholeLine: true
+			isWholeLine: true,
 		});
 		doNothingDecorationType = vscode.window.createTextEditorDecorationType({
 			isWholeLine: true,
-			backgroundColor: ""
+			backgroundColor: "",
 		});
 	};
 
@@ -102,7 +133,6 @@ export function activate(context: vscode.ExtensionContext) {
 
 	//从这里开始的许多核心函数都依赖于一些数学处理： 当前光标在哪个区间里？ 每个区间的上下界限在哪里？ 所以我写了一个同目录的mathematica.ts，提供了函数来处理这些数学问题。在继续往下阅读之前请先阅读那个文件。
 	const highLightCurrentBlock = () => {
-
 		let editor = vscode.window.activeTextEditor;
 		if (editor) {
 			var getLineObject = getLineNumbers(editor);
@@ -124,13 +154,11 @@ export function activate(context: vscode.ExtensionContext) {
 						//这里的indexOf可能会造成疑惑。用这个条件实际上是在判断光标的位置是否是任何一个分隔符的所在行，如果是的话就返回True。
 						editor.setDecorations(decorationType, []);
 						editor.setDecorations(doNothingDecorationType, decorationsArray);
-					}
-					else {
+					} else {
 						editor.setDecorations(decorationType, decorationsArray);
 						editor.setDecorations(doNothingDecorationType, []);
 					}
-				}
-				else {
+				} else {
 					editor.setDecorations(decorationType, decorationsArray);
 					editor.setDecorations(doNothingDecorationType, []);
 				}
@@ -141,7 +169,6 @@ export function activate(context: vscode.ExtensionContext) {
 				editor.setDecorations(doNothingDecorationType, []);
 			}
 		}
-
 	};
 	// 接下来监听鼠标位置的改变
 	vscode.window.onDidChangeTextEditorSelection(highLightCurrentBlock);
@@ -163,33 +190,28 @@ export function activate(context: vscode.ExtensionContext) {
 					if (array.indexOf(interval[0]) > -1) {
 						if (interval[0] == array[array.length - 1]) {
 							//如果是最后一行的分隔符，那么什么也不做
-						}
-						else {
+						} else {
 							//如果不是最后一行,那么往下一格首行移动
 							let newPosition = new vscode.Position(interval[1] + 2, 0);
 							let newSelection = new vscode.Selection(newPosition, newPosition);
 							editor.selection = newSelection;
 						}
-					}
-					else {
+					} else {
 						//如果是那种单行格子，那么处理方式和其它单元格一样
 						if (interval[1] == array[array.length]) {
 							//如果单元格是最后一格的话，那么什么也不做
-						}
-						else {
+						} else {
 							//如果还不是最后一格，那么就去到下边界的下一格
 							let newPosition = new vscode.Position(interval[1] + 2, 0);
 							let newSelection = new vscode.Selection(newPosition, newPosition);
 							editor.selection = newSelection;
 						}
 					}
-				}
-				else {
+				} else {
 					//以下，光标肯定处于单元格
 					if (interval[1] == array[array.length]) {
 						//如果单元格是最后一格的话，那么什么也不做
-					}
-					else {
+					} else {
 						//如果还不是最后一格，那么就去到下边界的下一格
 						let newPosition = new vscode.Position(interval[1] + 2, 0);
 						let newSelection = new vscode.Selection(newPosition, newPosition);
@@ -198,8 +220,7 @@ export function activate(context: vscode.ExtensionContext) {
 				}
 			}
 			//vscode.window.showInformationMessage("Down已经激活")
-		}
-		else {
+		} else {
 			// do nothing
 		}
 	};
@@ -215,23 +236,20 @@ export function activate(context: vscode.ExtensionContext) {
 				let interval = Interval(array, cursor.line);
 				if (interval[0] == 0) {
 					//什么也不做,因为处于第一段
-				}
-				else {
+				} else {
 					if (interval[0] == interval[1]) {
 						if (array.indexOf(interval[0]) > -1) {
 							//如果是分隔符,往上一格末行
 							let newPosition = new vscode.Position(interval[0] - 2, 0);
 							let newSelection = new vscode.Selection(newPosition, newPosition);
 							editor.selection = newSelection;
-						}
-						else {
+						} else {
 							//如果不是分隔符，那么当做单元格来处理,去到上一格的末行
 							let newPosition = new vscode.Position(interval[0] - 2, 0);
 							let newSelection = new vscode.Selection(newPosition, newPosition);
 							editor.selection = newSelection;
 						}
-					}
-					else {
+					} else {
 						//此时肯定在单元格之中
 						let newPosition = new vscode.Position(interval[0] - 2, 0);
 						let newSelection = new vscode.Selection(newPosition, newPosition);
@@ -256,43 +274,55 @@ export function activate(context: vscode.ExtensionContext) {
 				//分类讨论 因为如果代码格只有1格的话，也会返回相等
 				if (array.indexOf(interval[0]) > -1) {
 					//什么也不做
-				}
-				else {
+				} else {
 					//首先定义这一行的首位
 					let position1 = new vscode.Position(interval[0], 0);
 					//然后找到末尾
-					let finalCharacter = editor.document.lineAt(new vscode.Position(interval[1], 0)).text.length;
+					let finalCharacter = editor.document.lineAt(
+						new vscode.Position(interval[1], 0)
+					).text.length;
 					let position2 = new vscode.Position(interval[1], finalCharacter);
 					let newSelection = new vscode.Selection(position1, position2);
 					editor.selection = newSelection;
 				}
-			}
-			else {
-					//可以模仿上面的定义
-					let position1 = new vscode.Position(interval[0], 0);
-					//然后找到末尾
-					let finalCharacter = editor.document.lineAt(new vscode.Position(interval[1], 0)).text.length;
-					let position2 = new vscode.Position(interval[1], finalCharacter);
-					let newSelection = new vscode.Selection(position1, position2);
-					editor.selection = newSelection;
+			} else {
+				//可以模仿上面的定义
+				let position1 = new vscode.Position(interval[0], 0);
+				//然后找到末尾
+				let finalCharacter = editor.document.lineAt(
+					new vscode.Position(interval[1], 0)
+				).text.length;
+				let position2 = new vscode.Position(interval[1], finalCharacter);
+				let newSelection = new vscode.Selection(position1, position2);
+				editor.selection = newSelection;
 			}
 		}
 	};
 
 	//  以下是注册命令的部分
-	const registerChangeState = vscode.commands.registerCommand("code-block.ChangeState", ChangeState);
+	const registerChangeState = vscode.commands.registerCommand(
+		"code-block.ChangeState",
+		ChangeState
+	);
 	context.subscriptions.push(registerChangeState);
 
-	const registernavigateThroughBlocksDown = vscode.commands.registerCommand("code-block.navigateThroughBlocksDown", navigateThroughBlocksDown);
+	const registernavigateThroughBlocksDown = vscode.commands.registerCommand(
+		"code-block.navigateThroughBlocksDown",
+		navigateThroughBlocksDown
+	);
 	context.subscriptions.push(registernavigateThroughBlocksDown);
 
-	const registernavigateThroughBlocksUp = vscode.commands.registerCommand("code-block.navigateThroughBlocksUp", navigateThroughBlocksUp);
+	const registernavigateThroughBlocksUp = vscode.commands.registerCommand(
+		"code-block.navigateThroughBlocksUp",
+		navigateThroughBlocksUp
+	);
 	context.subscriptions.push(registernavigateThroughBlocksUp);
 
-	const registerselectCurrentBlock = vscode.commands.registerCommand("code-block.selectCurrentBlock", selectCurrentBlock);
+	const registerselectCurrentBlock = vscode.commands.registerCommand(
+		"code-block.selectCurrentBlock",
+		selectCurrentBlock
+	);
 	context.subscriptions.push(registerselectCurrentBlock);
-
-
 }
 
 // this method is called when your extension is deactivated
